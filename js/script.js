5dd56a7db83a9ec124ba92fa328f0f0c761c2a63
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-10 16:30:27 +0100

    search ok

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 var app = new Vue({
   el: `#app`,
   data: {
-
+    query:'',
+    apiKey: 'ebe10cc264f177fa09506aa0e10c3287',
+    lang: 'it-IT'
   },
   mounted(){
 
   },
   methods: {
 
+    // RICERCA CON CHIAMATA PER I FILM
+    search(){
+      axios
+      .get("https://api.themoviedb.org/3/search/movie",{
+        params: {
+          language: this.lang,
+          api_key: this.apiKey,
+          query: this.query
+        }
+      })
+      .then((result) =>{
+        console.log(result.data);
+      })
+    }
+    // FINE RICERCA
   }
 });
